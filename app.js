90e7955ec3705618baf4958e0a16eb68f4ae0038
Rahul <157468927+RahulBytesved@users.noreply.github.com>
2024-04-01 10:41:31 +0530

    Add

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const requestIp = require("request-ip");
 
-app.set('trust proxy', true); // To trust the proxy's IP address
+app.set("trust proxy", true); // To trust the proxy's IP address
 
-app.get('/', (req, res) => {
-  const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+app.get("/", (req, res) => {
+  const clientIP =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log("clientIP==>>>>>>>",clientIP);
+    console.log("req.ip==>>>>>", req.ip);
   res.send(`Client's IP address is: ${clientIP}`);
 });
 
+app.get("/test", (req, res) => {
+  const clientIp = requestIp.getClientIp(req);
+  console.log("clientIP==>>>>>>>",clientIp);
+    console.log("req.ip==>>>>>", req.ip);
+  res.send(`Client's IP address is from req: ${clientIp}`);
+});
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
