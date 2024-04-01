467f08054310aad90ada0f0a227ac21185e7df4f
Rahul <157468927+RahulBytesved@users.noreply.github.com>
2024-04-01 10:55:29 +0530

    Add -

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ app.set("trust proxy", true); // To trust the proxy's IP address
 app.get("/", (req, res) => {
   const clientIP =
     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("clientIP==>>>>>>>",clientIP);
-    console.log("req.ip==>>>>>", req.ip);
+  console.log("clientIP==>>>>>>>", clientIP);
+  console.log("req.ip==>>>>>", req.ip);
   res.send(`Client's IP address is: ${clientIP}`);
 });
 
 app.get("/test", (req, res) => {
   const clientIp = requestIp.getClientIp(req);
-  console.log("clientIP==>>>>>>>",clientIp);
-    console.log("req.ip==>>>>>", req.ip);
+  console.log("clientIP==>>>>>>>", clientIp);
+  console.log("req.ip==>>>>>", req.ip);
   res.send(`Client's IP address is from req: ${clientIp}`);
+});
+
+app.get("/test-test", (request, res) => {
+  const ip =
+    request.headers["cf-connecting-ip"] ||
+    request.headers["x-real-ip"] ||
+    request.headers["x-forwarded-for"] ||
+    request.socket.remoteAddress ||
+    "";
+  console.log("req.ip==>>>>>", ip);
+  res.send(`Client's IP address is from req: ${ip}`);
 });
 
 app.listen(3000, () => {
